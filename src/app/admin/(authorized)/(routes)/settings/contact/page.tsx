485a750d4e27c0d0274c577952d2d0b99d132b6b
asaber3030 <abdulrahmansaber70@gmail.com>
2025-02-22 20:49:15 +0200

    fix

--- src/app/admin/(authorized)/(routes)/settings/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getSettings } from "@/server/app";
-import { ContactSettings } from "../../../_components/settings/contat-settings";
-
-export default async function SettingsPage() {
-  const settings = await getSettings();
-
-  return <ContactSettings settings={settings} />;
-}
--- src/app/admin/(authorized)/(routes)/settings/social/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getSettings } from "@/server/app";
-import { SocialSettings } from "../../../_components/settings/social-settings";
-
-export default async function SocialPage() {
-  const settings = await getSettings();
-
-  return <SocialSettings settings={settings} />;
-}
